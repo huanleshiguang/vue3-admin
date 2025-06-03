5c26fcf069e74ce7ee2f52ab75cdf47c544eb23b
Joehall <joehall@foxmail.com>
2025-06-03 10:53:36 +0800

    feat

--- src/router/asyncModules/index.ts
+++ src/router/asyncModules/index.ts
@@ -6,14 +6,19 @@ const modulesFiles = import.meta.glob<ImportVueFileType>('../../views/**/*.vue')
 console.log('modulesFiles', modulesFiles);
 
 // generate components map  生成路由映射
+// Object.entries后的结果-------> key,value形式的数组
+// [
+//   ['../../views/home/index.vue', () => import('../../views/home/index.vue')],
+//   ['../../views/user/list.vue', () => import('../../views/user/list.vue')]
+// ]
 export const asyncRoutes = Object.entries(modulesFiles).reduce((routes, [url, importFn]) => {
+  //  过滤掉login和components下的文件，对key进行重新矫正只保留views后面的文件路径
   if (!/\/(views\/login|components)\//.test(url)) {
     const path = url.replace('../../views/', '').replace('.vue', '');
 
-    routes[path] = importFn;
+    routes[path] = importFn as ImportVueFileFnType;
   }
 
   return routes;
 }, {} as Recordable<ImportVueFileFnType>);
-
 // console.log('asyncRoutes', asyncRoutes);
